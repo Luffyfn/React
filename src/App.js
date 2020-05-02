df2322b9c15dbc2e438df3dc14ff8ef10f01000c
Luffyfn <faresnasfi@gmail.com>
2020-05-02 12:17:48 +0100

    Checkpoint React Done

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
-      <form class="form-inline">
-          <label class="sr-only" for="inlineFormInputName2">Name</label>
-          <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Full Name"/>
+      <form className="form-inline">
+          <label className="sr-only" for="inlineFormInputName2">Name</label>
+          <input type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Full Name"/>
 
-          <label class="sr-only" for="inlineFormInputGroupUsername2">E-mail</label>
-          <div class="input-group mb-2 mr-sm-2">
-              <div class="input-group-prepend">
-                  <div class="input-group-text">@</div>
+          <label className="sr-only" for="inlineFormInputGroupUsername2">E-mail</label>
+          <div className="input-group mb-2 mr-sm-2">
+              <div className="input-group-prepend">
+                  <div className="input-group-text">@</div>
               </div>
-              <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="E-mail"/>
+              <input type="text" className="form-control" id="inlineFormInputGroupUsername2" placeholder="E-mail"/>
           </div>
 
-          <div class="form-check mb-2 mr-sm-2">
-              <input class="form-check-input" type="checkbox" id="inlineFormCheck"/>
-              <label class="form-check-label" for="inlineFormCheck">Remember me</label>
+          <div className="form-check mb-2 mr-sm-2">
+              <input className="form-check-input" type="checkbox" id="inlineFormCheck"/>
+              <label className="form-check-label" for="inlineFormCheck">Remember me</label>
           </div>
 
-          <button type="submit" class="btn btn-primary mb-2">Sign in</button>
+          <button type="submit" className="btn btn-primary mb-2">Sign in</button>
       </form>
     ); 
 }
